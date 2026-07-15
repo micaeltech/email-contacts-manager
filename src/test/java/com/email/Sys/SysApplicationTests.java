c3c1b28f74afd99a55285ff0389881d9276c4e2a
micaeltech <mic83534@gmail.com>
2026-07-15 09:22:21 -0300

    fix: correct @Disabled annotation typo

--- src/test/java/com/email/Sys/SysApplicationTests.java
+++ src/test/java/com/email/Sys/SysApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disable("Requires external Postgres/Redis infraestructure not available in CI")
+@Disabled("Requires external Postgres/Redis infraestructure not available in CI")
 class SysApplicationTests {
 
 	@Test
